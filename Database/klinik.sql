62ea7542874148e8865b4236b5117370fcba5e7b
mfawwazzildan <mfawwazzildan@gmail.com>
2026-06-11 13:22:21 +0700

    Update Database

--- Database/klinik.sql
+++ Database/klinik.sql
@@ -22,7 +22,11 @@ CREATE TABLE IF NOT EXISTS `admin` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table klinik.admin: ~3 rows (approximately)
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+	(1, 'admins', '202cb962ac59075b964b07152d234b70'),
+	(5, 'admin_klinik1', '202cb962ac59075b964b07152d234b70'),
+	(6, 'ok', '202cb962ac59075b964b07152d234b70');
 
 -- Dumping structure for table klinik.dokter
 CREATE TABLE IF NOT EXISTS `dokter` (
@@ -33,7 +37,11 @@ CREATE TABLE IF NOT EXISTS `dokter` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table klinik.dokter: ~3 rows (approximately)
+INSERT INTO `dokter` (`id`, `nama`, `spesialis`, `no_hp`) VALUES
+	(5, 'dr. Muhammad Fawwaz Zildan ', 'Kulit', '0886767699'),
+	(7, 'dr. Andi', 'Kanker', '09293930494'),
+	(9, 'dr. Ilham Budiyanto', 'Tulang', '0833332992');
 
 -- Dumping structure for table klinik.pasien
 CREATE TABLE IF NOT EXISTS `pasien` (
@@ -44,7 +52,10 @@ CREATE TABLE IF NOT EXISTS `pasien` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table klinik.pasien: ~2 rows (approximately)
+INSERT INTO `pasien` (`id`, `nama`, `alamat`, `no_hp`) VALUES
+	(5, 'Rizkyss', 'Jakarta Barat ', '09383848'),
+	(6, 'Ahmad', 'Kalimantan Timur', '0938383838');
 
 -- Dumping structure for table klinik.pelayanan_medis
 CREATE TABLE IF NOT EXISTS `pelayanan_medis` (
@@ -61,7 +72,15 @@ CREATE TABLE IF NOT EXISTS `pelayanan_medis` (
   CONSTRAINT `pelayanan_medis_ibfk_2` FOREIGN KEY (`dokter_id`) REFERENCES `dokter` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table klinik.pelayanan_medis: ~7 rows (approximately)
+INSERT INTO `pelayanan_medis` (`id`, `pasien_id`, `dokter_id`, `tanggal`, `keluhan`, `biaya`) VALUES
+	(6, 6, 5, '2026-04-08', 'Gatal Perih', 100000),
+	(9, 5, 7, '2026-06-23', '122', 1233),
+	(10, 5, 5, '2026-06-01', 'sakit', 2040400),
+	(11, 6, 7, '2026-06-01', 'kdokdo', 300000),
+	(12, 5, 5, '2026-06-15', 'eqeq', 3333),
+	(13, 6, 5, '2026-06-30', 'aw', 10),
+	(14, 5, 5, '2026-06-01', 'jiwa', 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
